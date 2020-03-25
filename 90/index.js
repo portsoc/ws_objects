d9044d2780febb3dcc7f20dd2bd533e6867883ca
Rich Boakes <rjb@port.ac.uk>
2020-03-25 22:50:50 +0000

    add moveBy to Shape

---
+++ 90/index.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Adding functions to a superclass makes them available
+ * to all classes that extend it.
+ */
+
+import { Circle, Rectangle } from './classes.mjs';
+
+// get a handle on the drawing canvas
+const ctx = document.querySelector('canvas').getContext('2d');
+
+// Create circle and rectangle objects
+const shapes = [
+  new Rectangle(100, 50, 100, 200, 'crimson'),
+  new Rectangle(300, 150, 100, 200, 'steelblue'),
+  new Circle(150, 350, 50, `crimson`),
+  new Circle(350, 450, 50, `steelblue`),
+];
+
+for (const s of shapes) {
+  s.moveBy(200,50);
+  s.draw(ctx);
+}
